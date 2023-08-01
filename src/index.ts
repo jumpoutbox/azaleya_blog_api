057ad753cfbb2bad7cae69f89e71769f0afb531d
netoGermias <netogeremias019@gmail.com>
2023-08-01 23:54:27 +0100

    tsconfig was setting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { server} from './server/Server';
 
-server.listen(3003, () => console.log("Your Api is run on http://localhost:3003"));
+server.listen(process.env.PORT || 3003, () => console.log("Your Api is run on http://localhost:3003"));
 
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import { StatusCodes} from 'http-status-codes';
 
 const router = Router();
 
 router.get('/', (req, res) => {
       return res.send("Welcome to Our AZALEYA API Blog");
 });
+
+router.post("/postagem", (req, res) => {
+  console.log(req.body);
+
+  return res.status(StatusCodes.ACCEPTED).send(req.body);
+})
 
 interface Home {
     
